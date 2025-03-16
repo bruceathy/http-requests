14fada0ead3f698d5d867af4835f5ade451f8b7f
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-03-15 23:48:59 -0400

    up

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,11 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
   return (
     <Places
       title="Available Places"
