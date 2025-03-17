c162ea2f5afbb07b53ba5c5c78c3e7068dc41d2d
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-03-16 21:55:21 -0400

    up

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,17 +5,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const data = await response.json();
-      setAvailablePlaces(data.places);
-      setIsFetching(false);
-    }
+  // useEffect(() => {
+  //   async function fetchPlaces() {
+  //     setIsFetching(true);
+  //     const response = await fetch("http://localhost:3000/places");
+  //     const data = await response.json();
+  //     setAvailablePlaces(data.places);
+  //     setIsFetching(false);
+  //   }
 
-    fetchPlaces();
-  }, []);
+  //   fetchPlaces();
+  // }, []);
 
   return (
     <Places
